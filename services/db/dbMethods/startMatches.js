375de0519c2b33de0af4f9cdb363dc8023b50915
Anna Rogers <2muddybarefeet@gmail.com>
2016-02-05 22:04:24 +0000

    Update the start matches not yet tested for bugs

--- services/db/dbMethods/startMatches.js
+++ services/db/dbMethods/startMatches.js
@@ -5,6 +5,30 @@ var moment = require('moment');
 module.exports = function (knex) {
 
   var module = {};
+
+  //Select all pending matches that start on passed in date (when used this is looking for all matches starting today)
+  module.selectPendingMatches = function (date) {
+    console.log('finding pending matches');
+    return knex('matches')
+      .where('status', 'pending')
+      .then( function(matches) {
+        return matches.filter(function(match) {
+          return Date.parse(match.startdate) <= Date.parse(date);
+        });
+      });
+  };
+
+  //Decide if a match can become active or not depending on whether it has a chalengee
+  module.updateMatchState = function (matches) {
+    console.log('updating match states');
+    return Promise.map(matches, function (match) {
+      if( match.challengee ) {
+        return module.activateMatch(match);
+      } else {
+        return module.rejectMatch(match);
+      }
+    });
+  };
   
   var rejectMatch = function (match) {
     return knex('matches')
@@ -38,28 +62,29 @@ module.exports = function (knex) {
     });
   };
 
-  module.selectPendingMatches = function (date) {
-    return knex('matches')
-    .where('status', 'pending')
-    .orderBy('startdate', 'desc')
-    .then( function(matches) {
-      //now select any which start today
-      return matches.map(function (match) {
+
+  // module.selectPendingMatches = function (date) {
+  //   return knex('matches')
+  //   .where('status', 'pending')
+  //   .orderBy('startdate', 'desc')
+  //   .then( function(matches) {
+  //     //now select any which start today
+  //     return matches.map(function (match) {
         
-        var startToday = moment(match.startdate).isSame(moment( new Date('2016-02-11T14:30:00.000Z') ), "day");
+  //       var startToday = moment(match.startdate).isSame(moment( new Date('2016-02-11T14:30:00.000Z') ), "day");
 
-        if(startToday) {
-          if (!match.challengee) { //can't start as noone to play against
-            return rejectMatch(match);
-          } else {
-            console.log('---->', activateMatch(match));
-            return activateMatch(match);
-          }
-        }
+  //       if(startToday) {
+  //         if (!match.challengee) { //can't start as noone to play against
+  //           return rejectMatch(match);
+  //         } else {
+  //           console.log('---->', activateMatch(match));
+  //           return activateMatch(match);
+  //         }
+  //       }
 
-      });
-    });
-  };
+  //     });
+  //   });
+  // };
 
   return module;
 
--- workers/startMatches.js
+++ workers/startMatches.js
@@ -6,20 +6,31 @@ module.exports = function (services) {
   setTimeout(function () {
   //in the schedule table have a Daily_Update key in the table
   //check this every hour
-    services.db.metaTable.get("Latest_Stock_Update") //----------->CHANGE THIS WHEN WORKED OUT HOW!!!
-    .then(function (date) {
-      //want to insert a new row in the table Latest_Status_Update
-      var iscurrentDay = moment(new Date(date)).isSame(new Date(), "day");
-      if (!iscurrentDay) {
-        return services.db.startMatches.selectPendingMatches(date)
-        .then(function (pending) {
-          console.log('pending', pending);
-          return;
-        });
-      }
-
+    // services.db.metaTable.get("Latest_Stock_Update") //----------->CHANGE THIS WHEN WORKED OUT HOW!!!
+    // .then(function (date) {
+    //   //want to insert a new row in the table Latest_Status_Update
+    //   var iscurrentDay = moment(new Date(date)).isSame(new Date(), "day");
+    //   if (!iscurrentDay) {
+    //     return services.db.startMatches.selectPendingMatches(date)
+    //     .then(function (pending) {
+    //       console.log('pending', pending);
+    //       return;
+    //     });
+      //   }
+    console.log('kicking off match starting job');
+    //formulate date to look for
+    var date = new Date();
+    //get all matches that should start today
+    // var date = providedDate || new Date(today.getFullYear(), today.getMonth(), today.getDate(), 14, 30);
+    //get all matches that should start today
+    return services.db.startMatches.selectPendingMatches(date)
+    .then( function(matches){
+      return services.db.startMatches.updateMatchState(matches);
+    })
+    .catch( function (err) {
+      console.error(err);
     });
 
-  }, 3000);//60000
+  }, 3000);//60000 HOURLY 3600000ms
 
 };
